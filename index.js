706cce571981c5591e270a8af86bce7d6b066150
DaichiSaito <hrkedz@gmail.com>
2023-03-25 21:33:56 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION_ID,
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+import https from "https"
+import express from "express"
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get("/", (req, res) => {
+  res.sendStatus(200)
+})
+
+app.post("/webhook", async (req, res) => {
+  res.send("HTTP POST request sent to the webhook URL!")
+  // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
+  if (req.body.events[0].type === "message") {
+    const sentMessage = req.body.events[0].message.text
+    console.log(sentMessage)
+    const isFirstCharacterNewLine = sentMessage.charAt(0) === "\n"
+    if (!isFirstCharacterNewLine) return
+
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: sentMessage}],
+    });
+
+    // 文字列化したメッセージデータ
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": `${completion.data.choices[0].message.content}`
+        },
+      ]
+    })
+
+    // リクエストヘッダー
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
+    }
+
+    // リクエストに渡すオプション
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // リクエストの定義
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // エラーをハンドル
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // データを送信
+    request.write(dataString)
+    request.end()
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
